41adde38715f1d2f836aa8406b4974ad2311de25
dirksavage88 <dirksavage88@gmail.com>
2023-09-27 23:06:58 -0400

    fix to cam info publish (assign to loadcaminfo), added cam calibration yaml file parameter to grab url
    Signed-off-by: dirksavage88 <dirksavage88@gmail.com>

--- src/side_by_side_stereo_node.cpp
+++ src/side_by_side_stereo_node.cpp
@@ -63,7 +63,9 @@ class SplitImagePair : public rclcpp::Node
             this->declare_parameter("output_width", rclcpp::PARAMETER_INTEGER);
             this->declare_parameter("output_height", rclcpp::PARAMETER_INTEGER);
             this->declare_parameter("input_image_topic", rclcpp::PARAMETER_STRING);
-
+            this->declare_parameter("left_cam_calibration_file", "");
+            this->declare_parameter("right_cam_calibration_file", "");
+            
             std::string inputImageTopic = this->get_parameter("input_image_topic").as_string();
             
             RCLCPP_INFO(this->get_logger(), "input topic to stereo splitter=%s\n", inputImageTopic.c_str());
@@ -72,6 +74,8 @@ class SplitImagePair : public rclcpp::Node
             std::string right_output_image_topic = this->get_parameter("right_output_image_topic").as_string();
             std::string left_camera_info_topic = this->get_parameter("left_camera_info_topic").as_string();
             std::string right_camera_info_topic = this->get_parameter("right_camera_info_topic").as_string();
+            left_cam_calibration_file = this->get_parameter("left_cam_calibration_file").as_string();
+            right_cam_calibration_file = this->get_parameter("right_cam_calibration_file").as_string();
             auto outputWidth = this->get_parameter("output_width").as_int();
             auto outputHeight = this->get_parameter("output_height").as_int();
             
@@ -82,31 +86,41 @@ class SplitImagePair : public rclcpp::Node
             pub_l_ = it.advertise("left/image_raw", 1);
             pub_r_ = it.advertise("right/image_raw", 1);
 
+            // Cam info manager instances
+            camera_info_manager::CameraInfoManager cimright(this, "narrow_stereo/right", right_cam_calibration_file);
+            camera_info_manager::CameraInfoManager cimleft(this, "narrow_stereo/left", left_cam_calibration_file);
+
             // Load camera info
-            camera_info_manager::CameraInfoManager cimright(this);
-            camera_info_manager::CameraInfoManager cimleft(this);
+            cimleft.loadCameraInfo(left_cam_calibration_file);
+            cimright.loadCameraInfo(right_cam_calibration_file);
 
-            // // Get camera info
-            cimleft.getCameraInfo();
-            cimright.getCameraInfo(); 
-
-            cimleft.loadCameraInfo("");
-            cimright.loadCameraInfo("");    
+            // Check URL
+            cimleft.validateURL(left_cam_calibration_file);
+            cimright.validateURL(right_cam_calibration_file);
+            
+            // Get camera info
+            info_left_ = cimleft.getCameraInfo();
+            info_right_ = cimright.getCameraInfo(); 
+            
+            // Create cam info pubs
+            pub_cam_info_l_ = create_publisher<sensor_msgs::msg::CameraInfo>("left/camera_info", 1);
+            pub_cam_info_r_ = create_publisher<sensor_msgs::msg::CameraInfo>("right/camera_info", 1);
         }
+
 
         // Image capture callback.
         void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr & msg)
         {
 
             int outputWidth, outputHeight;
-            sensor_msgs::msg::CameraInfo info_left, info_right;
+
             
             // Created shared ptr to image 
             auto img = std::make_shared<sensor_msgs::msg::Image>(); 
             cv_bridge::CvImageConstPtr cvImg; 
             
             // Get double camera image. 
-            cvImg = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::MONO8);
+            cvImg = cv_bridge::toCvShare(msg, msg->encoding);
             cv::Mat image = cvImg->image;
             
             // If there are any subscribers to either output topic then publish images
@@ -114,6 +128,7 @@ class SplitImagePair : public rclcpp::Node
             if (pub_l_.getNumSubscribers() > 0u ||
                 pub_r_.getNumSubscribers() > 0u)
             {
+
                 // Define the relevant rectangles to crop.
                 cv::Rect leftROI, rightROI;
                 leftROI.y = rightROI.y = 0;
@@ -143,16 +158,16 @@ class SplitImagePair : public rclcpp::Node
                 cvImage.image = use_scaled ? leftScaled : leftImage;
                 img = cvImage.toImageMsg();
                 pub_l_.publish(img);
-                info_left.header.stamp = img->header.stamp;
-                info_left.header.frame_id = img ->header.frame_id;
-                pub_cam_info_l_->publish(info_left);
+                info_left_.header.stamp = img->header.stamp;
+                info_left_.header.frame_id = img ->header.frame_id;
+                pub_cam_info_l_->publish(info_left_);
 
                 cvImage.image = use_scaled ? rightScaled : rightImage;
                 img = cvImage.toImageMsg();
                 pub_r_.publish(img);
-                info_right.header.stamp = img->header.stamp;
-                info_right.header.frame_id = img->header.frame_id;
-                pub_cam_info_r_->publish(info_right);
+                info_right_.header.stamp = img->header.stamp;
+                info_right_.header.frame_id = img->header.frame_id;
+                pub_cam_info_r_->publish(info_right_);
 
             }
         }
@@ -161,11 +176,17 @@ class SplitImagePair : public rclcpp::Node
     private:
         //Publisher shared ptrs (formerly node handles)
         rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_img_l_, pub_img_r_;
-        rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr pub_cam_info_l_ = create_publisher<sensor_msgs::msg::CameraInfo>("left/camera_info", 1);
-        rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr pub_cam_info_r_ = create_publisher<sensor_msgs::msg::CameraInfo>("right/camera_info", 1);
+        // rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr pub_cam_info_l_ = create_publisher<sensor_msgs::msg::CameraInfo>("left/camera_info", 1);
+        // rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr pub_cam_info_r_ = create_publisher<sensor_msgs::msg::CameraInfo>("right/camera_info", 1);
         rclcpp::TimerBase::SharedPtr timer_ptr_;
         image_transport::Publisher pub_l_;
         image_transport::Publisher pub_r_;
+        std::string left_cam_calibration_file;
+        std::string right_cam_calibration_file;
+        rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr pub_cam_info_l_;//_ = create_publisher<sensor_msgs::msg::CameraInfo>("left/camera_info", 1);
+        rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr pub_cam_info_r_;// = create_publisher<sensor_msgs::msg::CameraInfo>("right/camera_info", 1);
+        sensor_msgs::msg::CameraInfo info_right_;// = std::make_shared<sensor_msgs::msg::CameraInfo>();
+        sensor_msgs::msg::CameraInfo info_left_;// = std::make_shared<sensor_msgs::msg::CameraInfo>();
 };
 
 int main(int argc, char** argv)
@@ -175,10 +196,10 @@ int main(int argc, char** argv)
     SplitImagePair split_im;
     SplitImagePair *splitim_ptr;
     splitim_ptr = &split_im;
-    auto cinfo_m = std::make_shared<rclcpp::Node>("caminfo_mng");
+
     auto rawimage = std::make_shared<rclcpp::Node>("image_sub_cb");
     image_transport::ImageTransport it(rawimage);
-    image_transport::Subscriber sub = it.subscribe("image_mono", 1, std::bind(&SplitImagePair::imageCallback, splitim_ptr, std::placeholders::_1));
+    image_transport::Subscriber sub = it.subscribe("image_raw", 1, std::bind(&SplitImagePair::imageCallback, splitim_ptr, std::placeholders::_1));
     
     rclcpp::spin(rawimage);
     rclcpp::shutdown();
